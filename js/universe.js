e1dad0266664b711da53b67429954d684e0d24b5
Masud Rana <si05@gtrbd.net>
2023-03-03 01:59:20 +0600

    add modal html file

--- js/universe.js
+++ js/universe.js
@@ -1,3 +1,4 @@
+// API data load by fetch
 const loadData = async () => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`;
     try {
@@ -10,21 +11,30 @@ const loadData = async () => {
     }
 }
 
-const displayData = (allData) =>{
+// display API data
+const displayData = (allData) => {
     const dataContainer = document.getElementById('data-container');
+    // display 6 items
+    const showAllData = document.getElementById('showAll-data');
+    if(allData.length > 6){
+        allData = allData.slice(0,6)
+        showAllData.classList.remove('d-none')
+    }
+    else{
+        showAllData.classList.add('d-none')
+    }
     allData.forEach(data => {
-        console.log(data)
         const dataDiv = document.createElement('div')
         dataDiv.classList.add('col')
         dataDiv.innerHTML = `
         <div class="card h-100">
-            <img class="p-3" src="${data.image}" class="card-img-top" alt="...">
+            <img class="p-3 img-fluid" src="${data.image}" class="card-img-top" alt="...">
                 <div class="card-body pb-0">
                     <h5 class="card-title">Features</h5>
                     <ul>
                         <li>${data.features[0]}</li>
                         <li>${data.features[1]}</li>
-                        <li>${data.features[2]}</li>
+                        <li>${data.features[2] ? data.features : 'Not available'}</li>
                     </ul>
                     <hr>
                 </div>
@@ -34,13 +44,32 @@ const displayData = (allData) =>{
                         <p>${data.published_in}</p>
                     </div>
                     <div>
-                        <button type="button" class="btn btn-outline-danger rounded-5"><i class="fa-solid fa-arrow-right"></i></button>
+                        <button onclick="load-detail()" type="button" class="btn btn-outline-danger rounded-5" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-arrow-right"></i></button>
                     </div>
                 </div>
         </div>
         `;
         dataContainer.appendChild(dataDiv)
     });
+    
+    //stop loader
+    toggleSpinner(false)
+}
+// show all data
+document.getElementById('showAll-btn').addEventListener('click',function(){
+    //start loader
+    toggleSpinner(true)
+    loadData()
+})
 
+//toggle Spinner 
+const toggleSpinner = isLoading =>{
+    const loaderSection = document.getElementById('loader')
+    if(isLoading){
+        loaderSection.classList.remove('d-none')
+    }
+    else{
+        loaderSection.classList.add('d-none')
+    }
 }
 loadData()
