0907a95b90d4a319642d42ae91fcd8c57d147f35
Masud Rana <si05@gtrbd.net>
2023-03-03 21:34:04 +0600

    display details data on modal

--- js/universe.js
+++ js/universe.js
@@ -78,5 +78,47 @@ const loadDetail = async (id) => {
     const data = await res.json()
     displayDetail(data.data)
 }
-
+const displayDetail = (data) => {
+    console.log(data)
+    const modalContainer = document.getElementById('modal-body');
+    modalContainer.innerHTML = `
+    <div class="card bg-danger-subtle m-2">
+            <div class="card-body">
+                <h5 class="card-title fw-bold">${data.description}</h5>
+            </div>
+            <div class="d-md-flex justify-content-evenly align-items-center text-center m-0">
+                <h6 class="text-success rounded bg-white mx-3 p-3">${data.pricing[0].price} ${data.pricing[0].plan}</h6>
+                <h6 class="text-warning-emphasis rounded bg-white mx-3 p-3">${data.pricing[1].price} ${data.pricing[1].plan}</h6>
+                <h6 class="text-info-emphasis rounded bg-white mx-3 p-3">${data.pricing[2].price ? data.pricing[2].price : 'Free Of Cost'}</h6>
+            </div>
+            <div class="d-flex justify-content-between m-3">
+                <div>
+                    <h5>Features</h5>
+                    <ul>
+                        <li>${data.features['1'].feature_name}</li>
+                        <li>${data.features['2'].feature_name}</li>
+                        <li>${data.features['3'].feature_name}</li>
+                    </ul>
+                </div>
+                <div>
+                    <h5>Integration</h5>
+                    <ul>
+                        <li>${data.integrations[0] ? data.integrations[0] : 'No data found'}</li>
+                        <li>${data.integrations[1] ? data.integrations[1] : 'No data found'}</li>
+                        <li>${data.integrations[2] ? data.integrations[2] : 'No data found'}</li>
+                    </ul>
+                </div>
+            </div>
+    </div>
+    <div>
+        <div class="card m-2">
+            <img class="p-3 img-fluid" src="${data.image_link[0]}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title text-center">${data.input_output_examples['0'].input}</h5>
+                <p class="card-text text-center">${data.input_output_examples['0'].output}</p>
+            </div>
+        </div>
+    </div>
+    `;
+}
 loadData()
